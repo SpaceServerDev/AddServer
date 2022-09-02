56064100b71b701c15332053a3b96896b72e6740
yurisi0212 <dev.yurisi@gmail.com>
2022-09-02 12:18:40 +0900

    fix design and input required

--- js/CreateLink.js
+++ js/CreateLink.js
@@ -2,7 +2,5 @@ function createLink() {
     if (create_form.create_name.value && create_form.create_ip.value && create_form.create_port.value) {
         var link = 'https://addserver.yurisi.space/?name=' + create_form.create_name.value + '&ip=' + create_form.create_ip.value + '&port=' + create_form.create_port.value;
         result_form.result.value = link;
-    } else {
-        alert('サーバー名、IP、アドレスの全てを入力してください');
     }
 }
